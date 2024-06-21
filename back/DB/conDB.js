01a4ada1c88794257fbb68b34fd86c28d853d05d
Raggad Ghassen <97407978+RGhassen@users.noreply.github.com>
2024-06-21 09:58:34 +0100

    Update conDB.js
    test db

--- back/DB/conDB.js
+++ back/DB/conDB.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const DBconnect = async ()=>{
     try {
         
-        await mongoose.connect(process.env.MONGO_URI,
+        await mongoose.connect(mongodb+srv://rg:TEST123@cluster0.0lbc9ru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0,
             {useNewUrlParser : true,
             useUnifiedTopology : true},()=>console.log("DB connected")) ;
         }
